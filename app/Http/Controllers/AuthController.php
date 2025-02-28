9ea9ba032757a1f3170588cb2d0ce95a1de834f1
Isroilov Azizbek <aziz16110904@gmail.com>
2025-02-28 09:16:42 +0500

    completed Backend Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create($request->validated());
+        $token = $user->createToken('api-token')->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('api-token')->plainTextToken;
+            return response()->json([
+                'user' => auth()->user(),
+                'token' => $token
+            ]);
+        }
+        return response()->json([
+            'message' => 'Invalid credentials'
+        ], 401);
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+}
